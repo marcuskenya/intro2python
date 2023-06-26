7ef4d826b6e41c1398ff7dfc93b5a53d03b83bf5
recyclerview <marcusmbugua1@gmail.com>
2023-06-26 09:46:47 +0300

    calculating my bmi

---
+++ bmi.py
@@ -0,0 +1,11 @@
+mass= input("Enter your mass in kilos")
+height=input("Enter your height in metres ")
+
+mass= float (mass)
+height = float (height)
+
+bmi= mass / height
+
+print(mass)
+print(height)
+print("Your BMI is",bmi,"kg/m2")

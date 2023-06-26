ea5b377b9ba6c9399b3889de19a151ace1cad647
recyclerview <marcusmbugua1@gmail.com>
2023-06-26 08:57:58 +0300

    finding area of a rectangle

---
+++ calc_area.py
@@ -0,0 +1,10 @@
+user_width=input("Enter the width value")
+width=int(user_width)
+
+user_length=input("Enter the length value")
+length=int(user_length)
+
+area= width * length
+print(width)
+print(length)
+print(area)

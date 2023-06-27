c31f5795b45152baa5297a740be7e2de0957f016
recyclerview <marcusmbugua1@gmail.com>
2023-06-27 10:10:26 +0300

    conditional statements

---
+++ betting.py
@@ -0,0 +1,14 @@
+value = int(input("Enter your desired number between 1 - 5"))
+
+if value == 1:
+    print("You have won a 1000bob")
+elif value == 2:
+    print("You have won a pavilion hp laptop")
+elif value == 3:
+    print("You have won an aston martin dbx707")
+elif value == 4:
+    print("You have won a ticket to visit Dubai")
+elif value == 5:
+    print("You have won yourself a 10 million dollar mansion")
+else :
+    print("Invalid input")

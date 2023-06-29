4abfb99a06785cb0ad7a01e6c22c5c291838599a
recyclerview <marcusmbugua1@gmail.com>
2023-06-29 10:36:38 +0300

    Made some changes to this file

---
+++ datastructures/Tuples.py
@@ -0,0 +1,6 @@
+# You cannot modify a tuple.
+# It keeps the order.
+# Uses the normal brackets.
+
+myteam = ("Marcus", "Kane", "R9", "CR7")
+print(myteam)
--- datastructures/lists.py
+++ datastructures/lists.py
@@ -1,6 +1,11 @@
-friends=["Marcus","Gloria","Sharon","Ted"]
-friends.append("Clement")
-friends.append("Maggie")
+# Uses the squared brackets.
+# It keeps the order.
+# Can be modified by adding, removing opr updating.
+
+friends = ["Marcus", "Gloria", "Sharon", "Ted", "AnnJoy"]
+
+# friends.append("Clement")
+# friends.append("Maggie")
 # friends.remove("Gloria")
 
-print(friends)
+print(friends[1:5])

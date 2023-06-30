c339adc63f53a05160a3c2c14ae5e0330a04ea0f
recyclerview <marcusmbugua1@gmail.com>
2023-06-30 08:57:22 +0300

    Set data structure in python

---
+++ datastructures/Sets.py
@@ -0,0 +1,7 @@
+# defined by curly brackets
+# order is not guaranteed
+# set will not allow duplicates
+
+myfriends = {"yunus", "Maggie", "Louis", "Tracey"}
+myfriends.add("yunus")
+print(myfriends)

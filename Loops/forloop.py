b41c16c188bc692ce6a617362af6de744771ec1d
recyclerview <marcusmbugua1@gmail.com>
2023-06-30 10:21:45 +0300

    made a new for loop file

--- Loops/forloop.py
+++ Loops/forloop.py
@@ -1,3 +1,6 @@
 friends = ["Marcus", "Gloria", "Sharon", "Ted", "AnnJoy", "Clement", "Manu"]
 for j in friends:
     print(j)
+    if j == "Clement":
+        break
+
---
+++ Loops/forloopex2.py
@@ -0,0 +1,5 @@
+friends_ages = {"Marcus": 18, "Jayden": 20, "Marie": 25}
+for j in friends_ages:
+    print(j)
+    if j == "Jayden":
+        break

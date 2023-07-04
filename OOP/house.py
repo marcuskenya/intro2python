f5d76aa86da7e253ffe0622a89a0c255940aa256
recyclerview <marcusmbugua1@gmail.com>
2023-07-04 10:14:20 +0300

    Demonstration about OOP

---
+++ OOP/house.py
@@ -0,0 +1,6 @@
+class House:
+    def __init__(self, owner, location, roof, doors):
+        self.owner = owner
+        self.location = location
+        self.roof = roof
+        self.doors = doors
---
+++ OOP/house_objects.py
@@ -0,0 +1,9 @@
+from house import House
+
+owner1 = House("Marcus", "Kikuyu", "Motisha roofs ", "Rango doors")
+owner2 = House("Aguero", "Kikuyu", "Tendo roofs", "Uluwezi doors")
+owner3 = House("Leroy", "Tatu City", "Sindio roofs", "Uwezi doors")
+
+print(owner1.owner, owner1.location, owner1.roof, owner1.doors)
+print(owner2.owner, owner2.location, owner2.roof, owner2.doors)
+print(owner3.owner, owner3.location, owner3.roof, owner3.doors)

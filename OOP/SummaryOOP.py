ccb23014c9706e4efe00e7c220045df7c15d0390
recyclerview <marcusmbugua1@gmail.com>
2023-07-04 10:20:56 +0300

    OOP summary

---
+++ OOP/SummaryOOP.py
@@ -0,0 +1,9 @@
+# OOP is a paradigm / way of writing code that makes it more efficient.
+# We have classes and objects.
+# A class is like a blueprint/ a plan.
+# Object is an instance of a class.
+
+# Inheritance
+# Polymorphism
+# Abstraction
+# Encapsulation
